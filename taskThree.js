e8e9fb9df6b7e37a3bd6688bee5686b8608443db
theusufructuary <pentamingforall@gmail.com>
2021-11-19 10:48:16 +0000

    first part of task three done

--- taskThree.js
+++ taskThree.js
@@ -1,7 +1,10 @@
 // Using `prompt`, get a user-inputted value for the player move. Then call your function with that value as the player move and the hard-coded computer move. Display the result using `alert`.
 
 //PLAN
-//
+//variables pointing to user input
+//make alert that displays result
+//call function with user input & preset computer move
+
 
 // This will be deemed as complete when you can input any move for the player move and hard-code any move for the computer move and see the correct result (1, 0, or -1) in the alert.
 
@@ -36,8 +39,8 @@ function getWinner(playerMove, computerMove){
     }
     
   
-let playerMove = "paper";
+let playerMove = prompt("rock, paper, or scissors?");
 let computerMove = "paper";
 
 let result = getWinner(playerMove,computerMove); //0
-console.log(result);
+alert("You chose: " + result + ". The computer chose: " + computerMove);
